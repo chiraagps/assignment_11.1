4402d6c86eaa07e5e29a48ab3dc755447cd00fa0
Chiraag <chiraagps123@gmail.com>
2021-07-15 18:09:12 +0530

    changes done

--- calculate.py
+++ calculate.py
@@ -10,10 +10,13 @@ from time import sleep
 from locators import *
 
 
-def cal():
+def test_calculate():
     driver = webdriver.Chrome()
     driver.get('http://demo.guru99.com/insurance/v1/index.php')
     driver.maximize_window()
+
+
+    #Login to the site
     email = driver.find_element(*LoginPageLocators.EMAIL_TEXT)
     email.send_keys("chiraagps@gmail.com")
     password = driver.find_element_by_id("password")
@@ -21,13 +24,30 @@ def cal():
     driver.find_element(*LoginPageLocators.LOGIN_BUTTON).click()
     print('')
     print('Login is successfull')
-    try:
-        driver.find_element(*ProfilePageLocators.LOGOUT_BUTTON)
-        print('Logout element present')
-    except:
-        print('Logout element not present')
-    print("URL is:",driver.current_url)
-    print("Title is:",driver.title)
+
+
+    #Click on Request Quotation
+    driver.find_element(*ProfilePageLocators.REQUEST_QUOTATION).click()
+
+
+    #Select the radio button for Windscreen repair as Yes
+    driver.find_element(*RequestQuotationLocators.RADIO_YES).click()
+
+    #Enter estimated value as 1000
+    est_value = driver.find_element(*RequestQuotationLocators.ESTVALUE_TEXT)
+    est_value.send_keys("1000")
+
+
+    #Select parking location as Public place
+    driver.find_element(*RequestQuotationLocators.DROP_PUBLIC).click()
+    
+    #Click on Calculate Premium button
+    driver.find_element(*RequestQuotationLocators.CAL_PREM).click()
+    sleep(2)
+
+    #Click on Reset Form
+    driver.find_element(*RequestQuotationLocators.RESET).click()
+    sleep(2)
     
 
 
--- locators.py
+++ locators.py
@@ -8,3 +8,10 @@ class LoginPageLocators(object):
 class ProfilePageLocators(object):
     LOGOUT_BUTTON = (By.XPATH,'//input[@value="Log out"]')
     REQUEST_QUOTATION = (By.XPATH,"//a[@id='ui-id-2']")
+
+class RequestQuotationLocators(object):
+    RADIO_YES = (By.XPATH,"//input[@id='quotation_windscreenrepair_t']")
+    ESTVALUE_TEXT = (By.XPATH,"//input[@id='quotation_vehicle_attributes_value']")
+    DROP_PUBLIC = (By.XPATH,"//select[@id='quotation_vehicle_attributes_parkinglocation']/option[text()='Public Place']")
+    CAL_PREM = (By.XPATH,"//input[@value='Calculate Premium']")
+    RESET = (By.XPATH,"//input[@id='resetquote']")
